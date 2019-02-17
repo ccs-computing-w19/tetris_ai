2fc11b361f5c1facc4a6cdceb94f2555c63cf793
d-guo <dguo@ucsb.edu>
2019-02-16 23:04:06 -0800

    added heightAI, which picks the best position based ont he resulting height, need to fix freezing bug when game is lost

---
+++ ai/heightAi.py
@@ -0,0 +1,19 @@
+import tetris
+from ai.utils.utils import isOutOfBounds
+from ai.utils.pathfinding import findPath
+from ai.utils.display import display
+
+# This ai chooses the position that gives the lowest height
+def choosePosition(board, positions):
+	bestPosition = [0]
+	bestPart = [0]
+
+	for pos in positions:
+		for part in pos:
+			if(part[0] > bestPart[0]):
+				if(not isOutOfBounds(board, part)):
+					bestPosition = pos
+					bestPart = part
+				break
+
+	return bestPosition
