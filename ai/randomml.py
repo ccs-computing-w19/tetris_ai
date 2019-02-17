e945f70f3351cb3b6a4a5db740b290d33a6c6b91
d-guo <dguo@ucsb.edu>
2019-02-16 23:27:24 -0800

    parameters for ml

--- ai/randomml.py
+++ ai/randomml.py
@@ -6,5 +6,30 @@ from ai.utils.display import display
 #this ai uses randomely assigned weights and biases to choose a position
 #later these will be updated after training
 
+#choose weights for
+#	height
+#	lowest hole
+#	holes
+#	filled lines (clear)
+#	touching neighbors
+
 def choosePosition(board, positions):
 	return positions[0]
+
+def height(board, position):
+
+
+def lowest(board, position):
+
+
+def holes(board, position):
+
+
+def clears(board, posiiton):
+
+
+def neighbors(board, position):
+
+
+def numWells(board, position):
+
