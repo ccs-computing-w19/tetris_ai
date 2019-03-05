c77916870b0a3f3af7a4340acd03e0885794790f
d-guo <dguo@ucsb.edu>
2019-03-04 18:56:57 -0800

    ml works boisss

--- ml.py
+++ ml.py
@@ -1,10 +1,9 @@
-import tetris
+from tetris.tetris import Tetris
 import random
 import numpy as np
 
-from ai.utils.utils import isOutOfBounds
+from ai.utils.utils import getActivePosition, findPositions, isOutOfBounds
 from ai.utils.pathfinding import findPath
-from ai.utils.display import display
 
 #use genetic algorithm to find weights for specified parametersto calculate which position to choose
 
@@ -26,38 +25,84 @@ from ai.utils.display import display
 CHANCEOFSURVIVAL = 0.2
 CHANCEOFMUTATE = 0.25
 INITIALPOP = 30
-DESIREDSCORE = 20
+DESIREDSCORE = 2
 
 class Player:
 	def __init__(self, weights = [0, 0, 0]):
-		wHeight = weights[0]
-		wNeighbors = weights[1]
-		wHoles = weights[2]
+		self.wHeight = weights[0]
+		self.wNeighbors = weights[1]
+		self.wHoles = weights[2]
 
 	def giveRandomWeights(self):
-		wHeight = random.uniform(-5, 5)
-		wNeighbors = random.uniform(-5, 5)
-		wHoles = random.uniform(-5, 5)
+		self.wHeight = random.uniform(-5, 5)
+		self.wNeighbors = random.uniform(-5, 5)
+		self.wHoles = random.uniform(-5, 5)
 
-	def findHeight(self, position, board):
-		pass
+	def findHeight(self, board):
+		height = 0
+		for r in range(len(board)):
+			if(1 in board[r]):
+				height = len(board) - r
+				break
+		return height
 
 	def numOfNeighbors(self, position, board):
-		pass
+		neighborCount = 0
+		for point in position:
+			if isOutOfBounds(board, (point[0] - 1, point[1])) or board[point[0] - 1][point[1]].isInactive(): neighborCount += 1
+			if isOutOfBounds(board, (point[0] + 1, point[1])) or board[point[0] + 1][point[1]].isInactive(): neighborCount += 1
+			if isOutOfBounds(board, (point[0], point[1] - 1)) or board[point[0]][point[1] - 1].isInactive(): neighborCount += 1
+			if isOutOfBounds(board, (point[0], point[1] + 1)) or board[point[0]][point[1] + 1].isInactive(): neighborCount += 1
+		return neighborCount
 
 	def numOfHoles(self, position, board):
-		pass
+		holes = 0
+		for r in range(len(board)):
+			for c in range(len(board[r])):
+				if(not isOutOfBounds(board, (r - 1, c)) and board[r-1][c].isInactive()):
+					holes += 1
+		return holes
 
-	def chooseMove(self, positions, board):
+	def choosePosition(self, positions, board):
 		positionScores = []
+		bestPosIndex = -1
 		for pos in positions:
-			positionScores.append(wHeight * findHeight(pos, board) + wNeighbors * numOfNeighbors(pos, board) + wHoles * numOfHoles(pos, board))
+			positionScores.append(self.wHeight * self.findHeight(board) + self.wNeighbors * self.numOfNeighbors(pos, board) + self.wHoles * self.numOfHoles(pos, board))
 		bestPosScore = max(positionScores)
 		numOfPositions = len(positions)
 		for i in range(numOfPositions):
 			if(positionScores[i] == bestPosScore):
-				bestPos = positions[i]
-		return bestPos
+				bestPosIndex = i
+		return bestPosIndex
+
+	def ai(self, game): #mutator function
+		board = game.getBoard()
+		position = getActivePosition(board, game.pivot)
+		positions = findPositions(board, position, game.rotatable)
+		path = None
+		while path == None:
+			# someday get around to fixing this stupid bug:
+			if len(positions) < 1:
+				path = []; break # set path to empty to deal with error
+			p = self.choosePosition(positions, board)
+			target = positions[p]
+			del positions[p] # remove from list of remaining positions
+			path = findPath(board, position, target, game.rotatable)
+		moves = path
+		# Essentially, handle input:
+		while len(moves) > 0:
+			if moves[0] == 'd':
+				del moves[0]
+				return
+			elif moves[0] == 'r':
+				del moves[0]
+				game.translateActiveRight()
+			elif moves[0] == 'l':
+				del moves[0]
+				game.translateActiveLeft()
+			elif moves[0] == 'u':
+				del moves[0]
+				game.rotateActiveClockwise()
 
 def create_initial_population(count):
 	#creates a population of players with random genomes
@@ -127,7 +172,8 @@ def evolve(pop):
 
 def playRound(game, player):
 	#player plays a around of the game and returns the updated score and state of game (ongoing or lost) return a tuple(game, score, gamestate)
-	pass
+	player.ai(game)
+	game.incrementTime()
 
 def train(pop):
 	#each player plays the game until death
@@ -136,15 +182,13 @@ def train(pop):
 	
 	highestScore = 0
 	for p in pop:
-		#make tetris object
-		#tetris = 0;
-		lost = false
+		game = Tetris()
+		lost = False
 		score = 0
-		while(!lost):
-			roundState = playRound(game, p)
-			lost = roundState[2]
-			score = roundState[1]
-			game = roundState[0]
+		while(not lost):
+			playRound(game, p)
+			lost = game.lost
+			score = game.numLines
 			if(score >= DESIREDSCORE):
 				return (score, p)
 		if(score > highestScore):
@@ -164,10 +208,12 @@ def main():
 		data = train(pop)
 		if(data[0] >= DESIREDSCORE):
 			final_player = data[1]
+			print("success")
 			break
 		else:
 			pop = data[1]
 
 	save(final_player)
 
+main()
 
